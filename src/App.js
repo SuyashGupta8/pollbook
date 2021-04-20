d87cae436366f25c5bd33506beaa43e485b7ffb1
SuyashGupta8 <suguptayash@gmail.com>
2021-04-21 01:40:03 +0530

    auth tested, working

--- src/App.js
+++ src/App.js
@@ -4,8 +4,11 @@ import React from 'react';
 import Header from './header/Header';
 import Login from './pages/Login';
 import 'bootstrap/dist/css/bootstrap.min.css';
+const axios = require('axios')
 
 class App extends React.Component {
+
+ 
   constructor(props) {
     super(props);
     this.state = { isLoggedIn: false };
@@ -17,6 +20,20 @@ class App extends React.Component {
   }
 
   render() {
+
+    axios.get("http://localhost:8080/login/test", {
+      // Axios looks for the `auth` option, and, if it is set, formats a
+      // basic auth header for you automatically.
+      auth: {
+        username: 'suyash',
+        password: 'suyash'
+      }
+    }).then((res)=>{
+      console.log("res obtained", res);
+    }).catch((err)=>{
+      console.log("error obtained", err);
+    });
+
     if (this.state.isLoggedIn) {
       return (
         <Header></Header>
